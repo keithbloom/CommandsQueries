b4592eb8f1038e611269884150273f84dce5bdf4
Keith Bloom <keith.bloom@gmail.com>
2014-01-27 11:27:31 +0000

    Added bad service

---
+++ CommandsQueries/Contracts/SearchCompanyForContracts.cs
@@ -0,0 +1,20 @@
+﻿namespace CommandsQueries.Contracts
+{
+    public class SearchCompanyForContracts : IQuery<CompanyContractsViewModel>
+    {
+        public int CompanyId { get; set; }
+    }
+
+    public class CompanyContractsViewModel
+    {
+        
+    }
+
+    public class SearchContractForCompanyHandler : IQueryHandler<SearchCompanyForContracts,CompanyContractsViewModel>
+    {
+        public CompanyContractsViewModel Handle(SearchCompanyForContracts request)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ CommandsQueries/ICompanyService.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace CommandsQueries
+{
+    public interface ICompanyService
+    {
+        int AddCompany(string name, string address1, int createdByPersonId);
+
+        void DeleteCompany(int id);
+
+        void AddContractForCompany(int companyId, int personAddedById, int clientId, string description, DateTime start);
+
+        void AddUserToCompany(int companyId, UserDto userDto);
+
+        PagedList<ContractListDto> ContractsForCompany(int companyId, int start, int page);
+
+        ContractDetailsDto GetContract(int contractId);
+
+        PagedList<ActiveClients> GetAllActiveClients(int companyId);
+    }
+
+    public class UserDto
+    {
+    }
+
+    public class ActiveClients
+    {
+    }
+
+    public class ContractDetailsDto
+    {
+    }
+
+    public class PagedList<T>
+    {
+    }
+
+    public class ContractListDto    
+    {
+    }
+}
